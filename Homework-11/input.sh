2b8c11e1ee40af2f3ec4d50b9123818b0525ec7c
buginets <adibaigaziev@gmail.com>
2025-02-28 16:35:49 +0000

    homework11

---
+++ Homework-11/input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "What is your name?  " name
+read -p "How old are you?  " age
+read -p "What is your favorite programming language?  " favorite_language
+read -p "What is your hobby?  " hobby
+read -p "What is your dream?  " dream
+
+
+echo "Hi , my name is $name, and i am $age years old. Every morning $name , likes to spend time doing $hobby. As a $age years old , it's never too late to follow your dreams. My favorite programming language is $favorite_language. I code in $favorite_language almost every day! One day i hope my love for $favorite_language wiil help me achieve my dream to $dream. When i am not busy , i am working toward my dream to $dream, you'll find me $hobby to relax. Thats a little about me, $name - a $age  -years old who loves $favorite_language, enjoys $hobby, and dreams of $dream "
---
+++ Homework-11/introduce.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=Adilet
+age=34
+favorite_language=JvaScript
+hobby=crossfit
+dream="travel around the world"
+
+echo "Hi , my name is $name, and i am $age years old. Every morning $name , likes to spend time doing $hobby. As a $age years old , it's never too late to follow your dreams. My favorite programming language is $favorite_language. I code in $favorite_language almost every day! One day i hope my love for $favorite_language wiil help me achieve my dream to $dream. When i am not busy , i am working toward my dream to $dream, you'll find me $hobby to relax. Thats a little about me, $name - a $age  -years old who loves $favorite_language, enjoys $hobby, and dreams of $dream "
